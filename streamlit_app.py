537afd6f7e1b9ddbb28294e1da7d3ed75070ce51
Kacper Masny <kacper.masny2000@gmail.com>
2024-01-27 20:35:32 +0100

    Add pre-processing and prediction functionality to the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,34 @@
 import streamlit as st
+import sklearn 
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import pickle
 import numpy as np
+import pandas as pd
 
 # loading the trained model
-# classifier = pickle.load(open('model.pkl', 'rb'))
-# transformer = pickle.load(open('transformer.pkl', 'rb'))
+classifier = pickle.load(open('model.pkl', 'rb'))
+transformer = pickle.load(open('preprocessor.pkl', 'rb'))
 
-# def predict(data: list):
-#     # Pre-processing user input
-#     data = np.array(data)
-#     transformed_data = transformer.transform(data.reshape(1, -1))
-#     prediction = classifier.predict(transformed_data)
-#     if prediction == 0:
-#         pred = 'You are not likely to experience a heart failure.'
-#     elif prediction == 1:
-#         pred = 'You are likely to experience a heart failure.'
+def predict(data):
+    # Pre-processing user input
+    data = pd.DataFrame(data=[data], columns=['Age', 'Sex', 'ChestPainType', 'RestingBP', 'Cholesterol', 'FastingBS',
+       'RestingECG', 'MaxHR', 'ExerciseAngina', 'Oldpeak', 'ST_Slope'])
+    data['Sex'] = data['Sex'].map({'Male':1, 'Female':0}).astype(int)
+    data['FastingBS'] = data['FastingBS'].map({'Yes':1, 'No':0}).astype(int)
+    data['ExerciseAngina'] = data['ExerciseAngina'].map({'Yes':1, 'No':0}).astype(int)
+    data['ChestPainType'] = data['ChestPainType'].map({'Typical Angina':'TA', 'Atypical Angina':'ATA', 'Non-anginal Pain':'NAP', 'Asymptomatic':'ASY'})
+    print(data)
+    transformed_data = transformer.transform(data)
+    prediction = classifier.predict(transformed_data)
+    if prediction == 0:
+        pred = 'You are not likely to experience a heart failure.'
+    elif prediction == 1:
+        pred = 'You are likely to experience a heart failure.'
     
-#     return pred
+    return pred
 
 
 def main():
@@ -27,6 +39,7 @@ def main():
     resting_blood_pressure = st.slider('What is your resting blood pressure?', 0, 200, 120)
     oldpeak = st.slider('What is your oldpeak?', 0.0, 10.0, 5.0)
     serum_cholestoral = st.slider('What is your serum cholestoral level?', 0, 600, 200)
+    maxHR = st.slider('What is your maximum heart rate achieved?', 0, 300, 150)
     sex = st.radio(
         "What is your Sex?",
         ["Male", "Female"]
@@ -49,13 +62,12 @@ def main():
     )
     slope = st.radio(
         "What is your slope of the peak exercise ST-segment?",
-        ["Upsloping", "Flat", "Downsloping"]
+        ["Up", "Flat", "Down"]
     )
     heart_failure = ''
     predict_button = st.button('Predict')
     if predict_button:
-        pass
-        # heart_failure = predict([age, resting_blood_pressure, oldpeak, serum_cholestoral, sex, chest_pain_type, fasting_blood_sugar, resting_electrocardiographic_results, exercise_induced_angina, slope])
+        heart_failure = predict([age, sex, chest_pain_type, resting_blood_pressure, serum_cholestoral, fasting_blood_sugar, resting_electrocardiographic_results, maxHR, exercise_induced_angina, oldpeak, slope])
 
     st.success(heart_failure)
     
